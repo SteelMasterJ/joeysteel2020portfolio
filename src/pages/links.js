8952b6e52ebccfe94c71e422a65bba137ab7b732
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-21 19:29:56 -0500

    Added Link

--- src/pages/links.js
+++ src/pages/links.js
@@ -4,7 +4,8 @@ import Layout from '../components/layout'
 const LinksPage = () => {
     return (
         <Layout>
-          <h1>These would be Links.</h1>
+          <h1>Links to stuff.</h1>
+          <a href="https://open.spotify.com/artist/4cncWMR1Es6HRLdr2rSW9e" rel="noreferrer" target="_blank" className="link-style">My Band "Slow Fix" On Spotify</a>
         </Layout>
     )
 }
