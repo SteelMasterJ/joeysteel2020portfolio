5920ad771cd51c69454d7ae1c8ebec7729599a0e
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-29 20:19:01 -0500

    functioning .md

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const Projects = () => {
+  const data = useStaticQuery(graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                subtitle
+              }
+            }
+          }
+        }
+      }
+    `)
+
     return (
         <Layout>
           <h1>Projects</h1>
           <p>Thanks for visiting my portfolio website. This site is a great starting point to showcase what web development skills (and a few life skills) I can bring to the table. Take a look around and please reach out to me directly if you would like to learn more!</p>
+          <ol>
+            {
+              data.allMarkdownRemark.edges.map((edge) => {
+                return (
+                  <li>
+                    <h2>{edge.node.frontmatter.title}</h2>
+                    <p>{edge.node.frontmatter.subtitle}</p>
+                  </li>
+                )
+              })
+            }
+          </ol>
         </Layout>
     )
 }
--- src/projects/testproject2.md
+++ src/projects/testproject2.md
@@ -1,6 +1,6 @@
 ---
-title: "Test Project"
-subtitle: "a test project to test things"
+title: "Test Project 2"
+subtitle: "a 2 test project 2 test things"
 ---
 
 A test project markdown post
