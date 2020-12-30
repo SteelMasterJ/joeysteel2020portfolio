b6560f57561d4c11a5ecceccff6d91ac309e210d
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2020-12-30 13:04:58 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
 
-    console.log(JSON.stringify(node, undefined, 4));
+    if(node.internal.type === 'MarkdownRemark') {
+        const slug = path.basename(node.fileAbsolutePath, '.md');
+        
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
+    }
+    //console.log(JSON.stringify(node, undefined, 4));
 }
