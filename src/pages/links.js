fe5fc2054907c3910aa2870e3f0c422b662c3aa6
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-01-22 19:07:05 -0500

    Updated Links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -28,12 +28,14 @@ const LinksPage = () => {
         <Layout>
             <Head title="Links" />
             <h1>Links to stuff.</h1>
-            <h4>My Resume</h4>
-            <Link to="https://drive.google.com/file/d/1zMw1cJu4z47vOK-JiAYSbt_Pm7L-EHKT/view?usp=sharing" target="_blank">
+            
+            <Link to="https://drive.google.com/file/d/1zMw1cJu4z47vOK-JiAYSbt_Pm7L-EHKT/view?usp=sharing" target="_blank" className="link-style">
+                <h4 style={{marginBottom: ".4rem", marginLeft: "1.4rem"}}>My Resume</h4>
                 <Img fluid={data.image2.childImageSharp.fluid} className='profile-pic' alt="CV" style={{border: "none"}}/>
             </Link>
-            <h4>My Band "Slow Fix" On Spotify</h4>
-            <Link to="https://open.spotify.com/artist/4cncWMR1Es6HRLdr2rSW9e" target="_blank">
+            
+            <Link to="https://open.spotify.com/artist/4cncWMR1Es6HRLdr2rSW9e" target="_blank" className="link-style">
+                <h4 style={{marginBottom: ".4rem"}}>My Band "Slow Fix" On Spotify</h4>
                 <Img fluid={data.image1.childImageSharp.fluid} className='profile-pic' alt="Slow Fix Promo"/>
             </Link>
         </Layout>
