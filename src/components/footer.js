e0d9575565010bc768fba25b331b6f442eab18fe
Joseph Steel <62897806+SteelMasterJ@users.noreply.github.com>
2021-01-26 17:04:25 -0500

    Added Footer Social Icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './footer.module.scss'
 import footerStyles from './footer.module.scss'
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
     return (
         <footer className={footerStyles.footer}>
-            <small className={footerStyles.small}>joeysteel.com ©2021</small>
+            <small className={footerStyles.small}>
+                joeysteel.com ©2021
+                <SocialIcon url="https://www.linkedin.com/in/joseph-steel-6271a955/" style={{ height: 30, width: 30, marginLeft: ".6rem"}}/>
+                <SocialIcon url="https://github.com/SteelMasterJ" style={{ height: 30, width: 30, marginLeft: ".6rem"}}/>
+                <SocialIcon url="mailto:joseph.steel.iv@gmail.com" style={{ height: 30, width: 30, marginLeft: ".6rem"}}/>
+            </small>
         </footer>
     )
 }
